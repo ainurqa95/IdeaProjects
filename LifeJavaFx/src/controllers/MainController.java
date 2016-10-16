3c74094d4059421775102bd61ee2456b276e27d5
ainur <ainur_ahmetgalie@mail.ru>
2016-10-16 11:57:03 +0400

    created main fxml

--- LifeJavaFx/src/controllers/MainController.java
+++ LifeJavaFx/src/controllers/MainController.java
@@ -1,6 +1,14 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
 public class MainController {
+    private Stage mainStage;
+    public void setMainStage(Stage mainStage) {
+        this.mainStage = mainStage;
+    }
 
-
+    public void actionButtonGo(ActionEvent actionEvent) {
+    }
 }
--- LifeJavaFx/src/start/Main.java
+++ LifeJavaFx/src/start/Main.java
@@ -1,5 +1,6 @@
 package start;
 
+import controllers.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,10 +12,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/main.fxml"));
+        FXMLLoader fmxLoader = new FXMLLoader(); // создаем объект загрузчика fxmlLoader
+        fmxLoader.setLocation(getClass().getResource("../views/main.fxml")); // привязываем его к xml
+        //  Parent root = FXMLLoader.load(getClass().getResource("../fxml/maket.fxml"));
+        Parent root = fmxLoader.load(); // загружаем его в root
+        MainController mainController = fmxLoader.getController(); // получаем контроллер нашей вьюхи xml
+        mainController.setMainStage(primaryStage); // засовываем его в главную сцену этот Stage мы можем использовать в контроллере
+        primaryStage.setMinHeight(300);
+        primaryStage.setMinWidth(500);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        //   showAdressBooksItems();
     }
 
 
