4cdfb46fe8ff68e2aeed2f2067a4330b3c0b37af
ainur <ainur_ahmetgalie@mail.ru>
2016-11-08 22:27:29 +0400

    MemoryStorage

--- mySite/src/ru/mysite/web/store/MemoryStorage.java
+++ mySite/src/ru/mysite/web/store/MemoryStorage.java
@@ -1,48 +1,61 @@
 package ru.mysite.web.store;
 
+import jdk.nashorn.internal.runtime.arrays.IteratorAction;
 import ru.mysite.web.interfaces.Storage;
 import ru.mysite.web.models.Users;
 
 import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by ainur on 08.11.16.
  */
 public class MemoryStorage implements Storage {
 
+    private final AtomicInteger ids = new AtomicInteger();
+    private final ConcurrentHashMap<Integer, Users> users = new ConcurrentHashMap<Integer, Users>();
+
     @Override
     public Collection<Users> values() {
-        return null;
+        return this.users.values();
     }
 
     @Override
-    public int add(Users user) {
-        return 0;
+    public int add(Users user) { // добавляем в коллекцию и получаем id
+         this.users.put(user.getId(), user);
+        return user.getId();
     }
 
     @Override
     public void edit(Users user) {
-
+        this.users.replace(user.getId(), user);
     }
 
     @Override
     public void delete(int id) {
-
+        this.users.remove(id);
     }
 
     @Override
-    public Users get(int id) {
-        return null;
+    public Users get(int id) { // получаем объект
+        return this.users.get(id);
     }
 
     @Override
     public Users findByLogin(String login) {
-        return null;
-    }
+        for (final Users user : users.values()) {
+            if (user.getLogin().equals(login)) {
+                return user;
+            }
 
+        }
+        throw new IllegalStateException(String.format("Login %s not found", login));
+    }
     @Override
     public int generateId() {
-        return 0;
+            return this.ids.incrementAndGet();
+
     }
 
     @Override
